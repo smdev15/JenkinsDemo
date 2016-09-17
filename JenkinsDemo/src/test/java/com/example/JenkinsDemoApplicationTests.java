d3f61522633fa0fe0d28d75e8fe819a94fad11f0
smdev15 <sumanm.82@gmail.com>
2016-09-16 23:24:26 -0600

    Initial Setup

---
+++ JenkinsDemo/src/test/java/com/example/JenkinsDemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void helloWorldTest() {
+		JenkinsDemoApplication demoApp = new JenkinsDemoApplication();
+		Assert.assertTrue(demoApp.message().equals("Hellow World"));
+	}
+}
