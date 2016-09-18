9b98e4bb4138c98932d17704e1b684aad85f437c
smdev15 <sumanm.82@gmail.com>
2016-09-17 22:42:11 -0600

    Updated pom

--- JenkinsDemo/src/main/java/com/example/JenkinsDemoApplication.java
+++ JenkinsDemo/src/main/java/com/example/JenkinsDemoApplication.java
@@ -5,9 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
+	private String str;
 	
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
 	public String message() {
-		String msg = "Hello World";
+		setStr("Hello World");
+		String msg = getStr();
 		return msg;
 	}
 
